7a33f55ed44b1eb63ff94b0ccc5a2589a8407c97
pandeminc <87105526+pandeminc@users.noreply.github.com>
2021-08-03 01:29:01 -0400

    Cargar archivos

---
+++ Config/Config.php
@@ -0,0 +1,35 @@
+<?php 
+	
+	//define("BASE_URL", "http://localhost/tienda_virtual/");
+	const BASE_URL = "http://localhost/sistema2";
+
+	//Zona horaria
+	date_default_timezone_set('America/Santiago');
+
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "db_sistema2";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "utf8";
+
+	//Deliminadores decimal y millar Ej. 24.198
+	const SPD = ".";
+	const SPM = ".";
+
+	//Simbolo de moneda
+	const SMONEY = "$";
+
+	//Datos envio de correo
+	const NOMBRE_REMITENTE = "Quotaware";
+	const EMAIL_REMITENTE = "no-reply@pandeminc.cl";
+	const NOMBRE_EMPESA = "IngeniosChile";
+	const WEB_EMPRESA = "www.ingenioschile.cl";
+	
+
+
+
+	
+
+
+ ?>
